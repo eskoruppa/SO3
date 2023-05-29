63586e58c663bb7a554a53f3a9378439877e6b47
esasen <esdrifter@gmail.com>
2023-05-29 13:35:24 +0200

    implemented linear expansion of Cayley to Euler transformation

--- so3/__init__.py
+++ so3/__init__.py
@@ -10,7 +10,10 @@ from .pyConDec.pycondec import cond_jit
 from .generators import hat_map, vec_map, generator1, generator2, generator3
 from .Cayley import cayley2rotmat, rotmat2cayley
 from .Euler import euler2rotmat, rotmat2euler
-from .conversions import cayley2euler, cayley2euler_factor, euler2cayley, euler2cayley_factor
+from .conversions import cayley2euler, cayley2euler_factor
+from .conversions import euler2cayley, euler2cayley_factor
+from .conversions import cayley2euler_linearexpansion
+
 
 # legacy method
 from .SO3Methods  import  phi2rotx, phi2roty, phi2rotz
--- so3/conversions.py
+++ so3/conversions.py
@@ -2,8 +2,8 @@
 
 import numpy as np
 from .pyConDec.pycondec import cond_jit
+# from .generators import hat_map, vec_map, generator1, generator2, generator3
 
-from .generators import hat_map, vec_map, generator1, generator2, generator3
 
 @cond_jit
 def cayley2euler(cayley: np.ndarray) -> np.ndarray: 
@@ -57,4 +57,27 @@ def euler2cayley_factor(euler: np.ndarray) -> float:
     """
     norm = np.linalg.norm(euler)
     return 2*np.tan(0.5*norm)/norm
+
+##########################################################################################################
+############### Linear Transformations based on linear expansion #########################################
+##########################################################################################################
+
+def cayley2euler_linearexpansion(cayley_gs: np.ndarray) -> np.ndarray:
+    """Linearization of the transformation from Cayley to Euler vector around a given groundstate vector
+
+    Args:
+        cayley_gs (np.ndarray): The Cayley vector around which the transformation is linearly expanded
+
+    Returns:
+        float: Linear transformation matrix that transforms small deviations around the given groundstate
+    """
+    cnorm = np.linalg.norm(cayley_gs)
+    csq = cnorm**2
+    fac = 2./csq*(2./(4+csq) - np.arctan(cnorm/2)/cnorm)
+    mat = np.zeros((3,3))
+    for i in range(3):
+        for j in range(3):
+            mat[i,j] = fac * cayley_gs[i] * cayley_gs[j]
+        mat[i,i] += 2*np.arctan(0.5*cnorm)/cnorm
+    return mat
 
