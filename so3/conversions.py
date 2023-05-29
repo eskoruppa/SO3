92f85d0a00c4ebfdbd742b75c043278967e8a582
esasen <esdrifter@gmail.com>
2023-05-29 20:50:38 +0200

    implemented conversion between lie algebra and lie group splitting of dynamic and static components

--- so3/conversions.py
+++ so3/conversions.py
@@ -63,6 +63,7 @@ def euler2cayley_factor(euler: np.ndarray) -> float:
 ############### Linear Transformations based on linear expansion #########################################
 ##########################################################################################################
 
+@cond_jit
 def cayley2euler_linearexpansion(cayley_gs: np.ndarray) -> np.ndarray:
     """Linearization of the transformation from Cayley to Euler vector around a given groundstate vector
 
@@ -82,6 +83,7 @@ def cayley2euler_linearexpansion(cayley_gs: np.ndarray) -> np.ndarray:
         mat[i,i] += 2*np.arctan(0.5*cnorm)/cnorm
     return mat
 
+@cond_jit
 def euler2cayley_linearexpansion(euler_gs: np.ndarray) -> np.ndarray:
     """Linearization of the transformation from Euler to Cayley vector around a given groundstate vector
 
@@ -106,6 +108,7 @@ def euler2cayley_linearexpansion(euler_gs: np.ndarray) -> np.ndarray:
 ############### Change Splitting between static and dynamic components ###################################
 ##########################################################################################################
 
+@cond_jit
 def splittransform_group2algebra(Theta_0: np.ndarray) -> np.ndarray:
     """
     Linear transformation that maps dynamic component in group splitting representation 
@@ -121,6 +124,7 @@ def splittransform_group2algebra(Theta_0: np.ndarray) -> np.ndarray:
         float: Linear transformation matrix T (3x3) that transforms Delta into Delta': T*Delta = Delta'
     """
     htheta = hat_map(Theta_0)
+    hthetasq = np.matmul(htheta,htheta)
     accutheta = np.copy(htheta)
     # first order
     T = np.eye(3)
@@ -130,27 +134,23 @@ def splittransform_group2algebra(Theta_0: np.ndarray) -> np.ndarray:
     accutheta = np.matmul(accutheta,htheta)
     T += 1./12 * accutheta
     # fifth order
-    accutheta = np.matmul(accutheta,htheta)
-    accutheta = np.matmul(accutheta,htheta)
+    accutheta = np.matmul(accutheta,hthetasq)
     T += -1./720 * accutheta
     # seventh order
-    accutheta = np.matmul(accutheta,htheta)
-    accutheta = np.matmul(accutheta,htheta)
+    accutheta = np.matmul(accutheta,hthetasq)
     T += 1./30240 * accutheta
     # ninth order
-    accutheta = np.matmul(accutheta,htheta)
-    accutheta = np.matmul(accutheta,htheta)
+    accutheta = np.matmul(accutheta,hthetasq)
     T += -1./1209600 * accutheta
     # eleventh order
-    accutheta = np.matmul(accutheta,htheta)
-    accutheta = np.matmul(accutheta,htheta)
+    accutheta = np.matmul(accutheta,hthetasq)
     T += 1./47900160 * accutheta
     # thirteenth order
-    accutheta = np.matmul(accutheta,htheta)
-    accutheta = np.matmul(accutheta,htheta)
+    accutheta = np.matmul(accutheta,hthetasq)
     T += -691./1307674368000 * accutheta
     return T
 
+@cond_jit
 def splittransform_algebra2group(Theta_0: np.ndarray) -> np.ndarray:
     """
     Linear transformation that maps dynamic component in lie algebra splitting representation R = exp(hat(Theta_0) + hat(Delta')) to group splitting representation 
