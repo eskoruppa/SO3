17a0b8a87e1db6a3f4f12ae94bf9028dd50700ec
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2024-01-16 20:45:13 +0100

    added SE3 midstep translation

--- so3/__init__.py
+++ so3/__init__.py
@@ -16,7 +16,7 @@ from .conversions import euler2cayley, euler2cayley_factor
 from .conversions import cayley2euler_linearexpansion, euler2cayley_linearexpansion
 from .conversions import splittransform_group2algebra, splittransform_algebra2group
 from .matrices import dots
-from .SE3 import se3_inverse, se3_triads2rotmat
+from .SE3 import se3_inverse, se3_triads2rotmat, se3_triadxrotmat_midsteptrans, se3_triads2rotmat_midsteptrans, sqrt_rot
 
 # legacy method
 from .SO3Methods  import  phi2rotx, phi2roty, phi2rotz
