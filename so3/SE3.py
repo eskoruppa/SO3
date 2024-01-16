b310a427bf7e4dcbb5d10f6ad0557191e8a22ddd
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2024-01-16 20:40:33 +0100

    added SE3 midstep translation

--- so3/SE3.py
+++ so3/SE3.py
@@ -9,7 +9,8 @@ from .Euler import euler2rotmat, rotmat2euler
 def se3_inverse(g: np.ndarray) -> np.ndarray:
     inv = np.zeros(g.shape)
     inv[:3,:3] = g[:3,:3].T
-    inv[:3,3]  = -inv[:3,:3]@g[:3,3]
+    inv[:3,3]  = -np.dot(inv[:3,:3],g[:3,3])
+    # inv[:3,3]  = -inv[:3,:3]@g[:3,3]
     inv[3,3] = 1
     return inv
 
@@ -22,17 +23,22 @@ def se3_triadxrotmat_midsteptrans(tau1: np.ndarray, g: np.ndarray) -> np.ndarray
     R = g[:3,:3]
     T1 = tau1[:3,:3]
     tau2 = np.eye(4)
-    tau2[:3,:3] = T1 @ R
-    tau2[:3,3] = tau1[:3,3] + T1 @ sqrt_rot(R) @ g[:3,3]
+    tau2[:3,:3] = np.dot(T1, R)
+    # tau2[:3,:3] = T1 @ R
+    tau2[:3,3] = tau1[:3,3] + np.dot(np.dot(T1, sqrt_rot(R)), g[:3,3])
+    # tau2[:3,3] = tau1[:3,3] + T1 @ sqrt_rot(R) @ g[:3,3]
     return tau2
 
 @cond_jit
 def se3_triads2rotmat_midsteptrans(tau1: np.ndarray, tau2: np.ndarray) -> np.ndarray:
     T1 = tau1[:3,:3]
     T2 = tau2[:3,:3]
-    R = T1.T @ T2
-    Tmid = T1 @ sqrt_rot(R)
-    zeta = Tmid.T @ (tau2[:3,3]-tau1[:3,3])
+    R = np.dot(T1.T , T2)
+    Tmid = np.dot(T1 , sqrt_rot(R))
+    zeta = np.dot(Tmid.T, (tau2[:3,3]-tau1[:3,3]))
+    # R = T1.T @ T2
+    # Tmid = T1 @ sqrt_rot(R)
+    # zeta = Tmid.T @ (tau2[:3,3]-tau1[:3,3])
     g = np.ones(4)
     g[:3,:3] = R
     g[:3,3]  = zeta
